6752ceae71aed39ba3818f6d95e715098416a50d
MiladKh <milad_khaleghi@live.com>
2019-06-01 04:11:03 +0430

    init Windows Client:not work to keydown at the same time

---
+++ Client_PC_Version/ClientForPC/ClientForPC/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace ClientForPC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblState = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblState});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 239);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(284, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblState
+            // 
+            this.lblState.Name = "lblState";
+            this.lblState.Size = new System.Drawing.Size(49, 17);
+            this.lblState.Text = "nothing";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblState;
+    }
+}
+
---
+++ Client_PC_Version/ClientForPC/ClientForPC/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+
+using System.Net;
+using System.Net.Sockets;
+
+namespace ClientForPC
+{
+    public partial class Form1 : Form
+    {
+        String ip_address = "192.168.4.1";
+        int port = 23;
+        Socket socket;
+
+        void Bind()
+        {
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress ip = IPAddress.Parse(ip_address);
+            IPEndPoint ipep = new IPEndPoint(ip, port);
+
+            socket.Connect(ipep);
+        }
+
+        void Exit()
+        {
+            socket.Close();
+            socket = null;
+        }
+
+        void Send(String msgData)
+        {
+            if (socket == null)
+                return;
+
+            byte[] sendData = Encoding.ASCII.GetBytes(msgData);
+            socket.Send(sendData);
+        }
+
+        String Receive()
+        {
+            byte[] receiveData = new byte[256];
+            socket.Receive(receiveData);
+            String result = Encoding.UTF8.GetString(receiveData);
+
+            return result;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            KeyDown += new KeyEventHandler(Form1_KeyDown);
+            KeyUp += new KeyEventHandler(Form1_KeyUp);
+        }
+        //protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        //{
+        //    //capture up arrow key
+        //    if (keyData == Keys.Up)
+        //    {
+        //        lblState.Text = "You pressed Up arrow key";
+
+        //        return true;
+        //    }
+        //    //capture down arrow key
+        //    if (keyData == Keys.Down)
+        //    {
+        //        lblState.Text = "You pressed Down arrow key";
+        //        return true;
+        //    }
+        //    //capture left arrow key
+        //    if (keyData == Keys.Left)
+        //    {
+        //        lblState.Text = "You pressed Left arrow key";
+        //        return true;
+        //    }
+        //    //capture right arrow key
+        //    if (keyData == Keys.Right)
+        //    {
+        //        lblState.Text = "You pressed Right arrow key";
+        //        return true;
+        //    }
+        //    return base.ProcessCmdKey(ref msg, keyData);
+        //}
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Bind();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            
+            if (e.KeyCode == Keys.Up)
+            {
+                
+                Send("2");
+                lblState.Text = "You pressed Up arrow key";
+            }
+            //capture down arrow key
+            if (e.KeyCode == Keys.Down)
+            {
+                
+                Send("1");
+                lblState.Text = "You pressed Down arrow key";
+            }
+            //capture left arrow key
+            if (e.KeyCode == Keys.Left)
+            {
+                
+                Send("3");
+                lblState.Text = "You pressed Left arrow key";
+            }
+            //capture right arrow key
+            if (e.KeyCode == Keys.Right)
+            {
+                
+                Send("4");
+                lblState.Text = "You pressed Right arrow key";
+            }
+            if ( e.KeyCode==Keys.Space)
+            {
+                Send("0");
+                lblState.Text = "salam";
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            
+            Send("0");
+            lblState.Text = "Normal State";
+        }
+    }
+}
