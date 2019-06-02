d8087e42cafee011a529ccd929690a2a62c2e149
MiladKh <milad_khaleghi@live.com>
2019-06-02 23:12:48 +0430

    full control but have issue in key up one of the button when you key down both of them

--- Client_PC_Version/ClientForPC/ClientForPC/Form1.cs
+++ Client_PC_Version/ClientForPC/ClientForPC/Form1.cs
@@ -19,14 +19,22 @@ namespace ClientForPC
         String ip_address = "192.168.4.1";
         int port = 23;
         Socket socket;
-
+        int isPressd = 0;
+        uint buttonPress1 = 0;
+        uint buttonPress2 = 0;
         void Bind()
         {
             socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             IPAddress ip = IPAddress.Parse(ip_address);
             IPEndPoint ipep = new IPEndPoint(ip, port);
 
-            socket.Connect(ipep);
+            try
+            {
+                socket.Connect(ipep);
+            }
+            catch { }
+            
+
         }
 
         void Exit()
@@ -41,7 +49,16 @@ namespace ClientForPC
                 return;
 
             byte[] sendData = Encoding.ASCII.GetBytes(msgData);
-            socket.Send(sendData);
+            try
+            {
+                socket.Send(sendData);
+            }
+            catch 
+            {
+
+                
+            }
+            
         }
 
         String Receive()
@@ -58,82 +75,127 @@ namespace ClientForPC
             KeyDown += new KeyEventHandler(Form1_KeyDown);
             KeyUp += new KeyEventHandler(Form1_KeyUp);
         }
-        //protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
-        //{
-        //    //capture up arrow key
-        //    if (keyData == Keys.Up)
-        //    {
-        //        lblState.Text = "You pressed Up arrow key";
 
-        //        return true;
-        //    }
-        //    //capture down arrow key
-        //    if (keyData == Keys.Down)
-        //    {
-        //        lblState.Text = "You pressed Down arrow key";
-        //        return true;
-        //    }
-        //    //capture left arrow key
-        //    if (keyData == Keys.Left)
-        //    {
-        //        lblState.Text = "You pressed Left arrow key";
-        //        return true;
-        //    }
-        //    //capture right arrow key
-        //    if (keyData == Keys.Right)
-        //    {
-        //        lblState.Text = "You pressed Right arrow key";
-        //        return true;
-        //    }
-        //    return base.ProcessCmdKey(ref msg, keyData);
-        //}
         private void Form1_Load(object sender, EventArgs e)
         {
             Bind();
         }
+        public void CheckKeyUp(uint keyNumber)
+        {
+            uint curretntKey = buttonPress2;
+            //MessageBox.Show(keyNumber.ToString());
+            //MessageBox.Show(curretntKey.ToString());
+            if (buttonPress2 != (uint)0) //takdokme gereft bod 
+            {
+                if (buttonPress2==curretntKey)
+                {
+                    buttonPress2 = (uint)0;
+                }
+                else if ((buttonPress1 == curretntKey))
+                {
+                    buttonPress1 = (uint)buttonPress2;
+                    buttonPress2 = (uint)0;
+                }
+               
+                
+            
+            }
+            else if (buttonPress2 == (uint)0)
+            {
+                buttonPress1 = (uint)0;
 
+            }
+            
+            lblState.Text = string.Format("you press {0} and {1}", buttonPress1.ToString(), buttonPress2.ToString());
+        }
+        public void CheckBeforePress(uint keyNumber)
+        {
+            if (buttonPress1 == 0) //dokmei ghablesh feshar dade ya na
+            {
+                buttonPress1 = keyNumber;
+                isPressd = 1;
+            }
+            else if(buttonPress1 != 0 && buttonPress1!=keyNumber)
+            {
+                buttonPress2 = keyNumber;
+                isPressd = 1;
+            }
+            
+            lblState.Text = string.Format("you press {0} and {1}", buttonPress1.ToString(), buttonPress2.ToString());
+        }
+        public void Move()
+        {
+        // main Dirction
+          if(buttonPress1==1 && buttonPress2==0) Send("1");
+          if(buttonPress1==2 && buttonPress2==0) Send("2");
+          if(buttonPress1==3 && buttonPress2==0) Send("3");
+          if(buttonPress1==4 && buttonPress2==0) Send("4");
+
+          if(buttonPress1==1 && buttonPress2==3) Send("5");
+          if (buttonPress1==3 && buttonPress2==1) Send("5");
+
+          if(buttonPress1==1 && buttonPress2==4) Send("6");
+          if(buttonPress1 == 4 && buttonPress2 == 1) Send("6");
+
+          if (buttonPress1==2 && buttonPress2==3) Send("7");
+          if (buttonPress1 == 3 && buttonPress2 == 2) Send("7");
+
+          if (buttonPress1==2 && buttonPress2==4) Send("8");
+          if(buttonPress1==4 && buttonPress2==2) Send("8");
+
+          if (buttonPress1 == 0 && buttonPress2 == 0) Send("0");
+        }
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
             
-            if (e.KeyCode == Keys.Up)
-            {
-                
-                Send("2");
-                lblState.Text = "You pressed Up arrow key";
-            }
+            if (e.KeyCode == Keys.Up) CheckBeforePress(2);
+
             //capture down arrow key
-            if (e.KeyCode == Keys.Down)
-            {
-                
-                Send("1");
-                lblState.Text = "You pressed Down arrow key";
-            }
+            if (e.KeyCode == Keys.Down) CheckBeforePress(1);
+
             //capture left arrow key
-            if (e.KeyCode == Keys.Left)
-            {
-                
-                Send("3");
-                lblState.Text = "You pressed Left arrow key";
-            }
+            if (e.KeyCode == Keys.Left) CheckBeforePress(3);
+
             //capture right arrow key
-            if (e.KeyCode == Keys.Right)
-            {
-                
-                Send("4");
-                lblState.Text = "You pressed Right arrow key";
-            }
-            if ( e.KeyCode==Keys.Space)
-            {
-                Send("0");
-                lblState.Text = "salam";
-            }
+            if (e.KeyCode == Keys.Right) CheckBeforePress(4);
+
+            if ( e.KeyCode==Keys.Space) Send("0");
+
+            Move();
         }
 
         private void Form1_KeyUp(object sender, KeyEventArgs e)
         {
+            //if (isPressd == 1)
+            //{
+            //    Send("0");
+            //    isPressd = 0;
+            //}
+            if (true)//(buttonPress2 > 0 && buttonPress1 > 0)
+            {
+                if (e.KeyCode == Keys.Up) CheckKeyUp(2);
+
+                //capture down arrow key
+                if (e.KeyCode == Keys.Down) CheckKeyUp(1);
+
+                //capture left arrow key
+                if (e.KeyCode == Keys.Left) CheckKeyUp(3);
+
+                //capture right arrow key
+                if (e.KeyCode == Keys.Right) CheckKeyUp(4);
+
+
+                isPressd++;
+                Move();
+            }
+
             
-            Send("0");
-            lblState.Text = "Normal State";
+           
+             
+            //lblState.Text = string.Format("you press {0} and {1}", buttonPress1.ToString(), buttonPress2.ToString());
+
+
+
         }
     }
 }
